4567320322fe8c20a8c73f0e622246ed3eff57ed
Hilton Shumway <hillshum@gmail.com>
2015-01-16 23:21:53 -0700

    Initial commit.
    Nothing seems to do much at this point.

---
+++ background.js
@@ -0,0 +1,19 @@
+
+
+chromse.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: 'ilearn.brainhoney' },
+                    })
+                ],
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
+    });
+});
+
+    
+     
